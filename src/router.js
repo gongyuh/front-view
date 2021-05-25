5b0d32917df81b8e617b9e6f05ea611b7194bdb3
gongyuh <2736498007@qq.com>
2021-05-25 16:15:03 +0800

    个人中心

--- src/router.js
+++ src/router.js
@@ -6,13 +6,15 @@ import Home from'./views/Home.vue'
 const Login = () => import('./views/Login.vue')
 const Reg = () => import('./views/Reg.vue')
 const Forget = () => import('./views/Forget.vue')
-const Index = () =>
-  import(/* webpackChunkName: 'index' */ './views/channels/Index.vue')
-const Template1 = () =>
-  import(/* webpackChunkName: 'template1' */ './views/channels/Template1.vue')
-const Center = () => {
-  import ('./views/Center.vue')
-}
+const Center = () => import ('./views/Center.vue')
+const Index = () =>import(/* webpackChunkName: 'index' */ './views/channels/Index.vue')
+const Template1 = () =>import(/* webpackChunkName: 'template1' */ './views/channels/Template1.vue')
+const UserCenter = () => import('./components/user/Center')
+const Settings = () => import('./components/user/Settings')
+const Posts = () => import('./components/user/Posts')
+const Msg = () => import('./components/user/Msg')
+const Others = () => import('./components/user/Others')
+const User = () => import ('./views/User.vue')
 
 Vue.use(VueRouter)
 
@@ -51,15 +53,49 @@ const routes = [
     }
   },
   {
-    path: '/center',
-    name: 'center',
-    component: Center
-  },
-  {
     path: '/forget',
     name: 'forget',
     component: Forget
   },
+  {
+    path: '/user/:uid',
+    name: 'user',
+    props: true,
+    component: User
+  },
+  {
+    path: '/center',
+    name: 'center',
+    component: Center,
+    linkActiveClass: 'layui-this',
+    children: [
+      {
+        path: '',
+        name: 'centet',
+        component: UserCenter
+      },
+      {
+        path: '/settings',
+        name: 'settings',
+        component: Settings
+      },
+      {
+        path: '/msg',
+        name: 'msg',
+        component: Msg
+      },
+      {
+        path: '/posts',
+        name: 'posts',
+        component: Posts
+      },
+      {
+        path: '/others',
+        name: 'others',
+        component: Others
+      },
+    ]
+  }
 ]
 
 
